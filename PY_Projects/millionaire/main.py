31e3c26af614ee2c34d030762ca411a85e5a1fc3
Ritesh Mane <riteshmane8308@gmail.com>
2025-07-27 10:28:04 +0530

    Adding KBC project

--- PY_Projects/millionaire/main.py
+++ PY_Projects/millionaire/main.py
@@ -1,20 +1,23 @@
 questions = [
-    ["Who is Shah Rukh Khan?", "WWE Wrestler", "Plumber", "Actor", "Astronaut", 3],
-    ["What is the capital of France?", "Berlin", "Paris", "Rome", "London", 2],
-    ["Which planet is known as the Red Planet?", "Earth", "Venus", "Mars", "Jupiter", 3],
-    ["What is the largest mammal?", "Shark", "Blue Whale", "Elephant", "Giraffe", 2],
-    ["Who wrote 'Romeo and Juliet'?", "William Shakespeare", "Jane Austen", "Charles Dickens", "Homer", 1],
-    ["What is the square root of 64?", "8", "10", "6", "12", 1],
-    ["Which country is known as the Land of the Rising Sun?", "India", "South Korea", "Japan", "China", 3],
-    ["Who painted the Mona Lisa?", "Claude Monet", "Pablo Picasso", "Leonardo da Vinci", "Vincent van Gogh", 3],
-    ["What is the fastest land animal?", "Horse", "Lion", "Cheetah", "Elephant", 3],
-    ["Which ocean is the largest?", "Indian Ocean", "Pacific Ocean", "Atlantic Ocean", "Arctic Ocean", 2],
-    ["What is the smallest country in the world?", "San Marino", "Vatican City", "Monaco", "Liechtenstein", 2]
+    ["Who is Shah Rukh Khan?", "WWE Wrestler", "Plumber", "Actor", "Astronaut", "c"],
+    ["What is the capital of France?", "Berlin", "Paris", "Rome", "London", "b"],
+    ["Which planet is known as the Red Planet?", "Earth", "Venus", "Mars", "Jupiter", "c"],
+    ["What is the largest mammal?", "Shark", "Blue Whale", "Elephant", "Giraffe", "b"],
+    ["Who wrote 'Romeo and Juliet'?", "William Shakespeare", "Jane Austen", "Charles Dickens", "Homer", "a"],
+    ["What is the square root of 64?", "8", "10", "6", "12", "a"],
+    ["Which country is known as the Land of the Rising Sun?", "India", "South Korea", "Japan", "China", "c"],
+    ["Who painted the Mona Lisa?", "Claude Monet", "Pablo Picasso", "Leonardo da Vinci", "Vincent van Gogh", "c"],
+    ["What is the fastest land animal?", "Horse", "Lion", "Cheetah", "Elephant", "c"],
+    ["Which ocean is the largest?", "Indian Ocean", "Pacific Ocean", "Atlantic Ocean", "Arctic Ocean", "b"],
+    ["What is the smallest country in the world?", "San Marino", "Vatican City", "Monaco", "Liechtenstein", "b"]
 ]
 
 prizes = [100000, 320000, 400000, 450000,  500000, 1000000, 2000000, 3000000, 4000000, 5000000, 6000000]
 
-i = 0 
+#Initializing values of variables:
+i = 0
+sum = 0
+
 for question in questions:
     print(question[0])
     print(f"a. {question[1]}")
@@ -22,13 +25,17 @@ for question in questions:
     print(f"c. {question[3]}")
     print(f"d. {question[4]}")
 
-    # Check whether the answer is correct or not
-    a = int(input("Enter your answer. 1 for a, 2 for b, 3 for c, 4 for d\n"))
+    # Check whether the answer is correct or not:
+    a = str(input("Enter your answer: "))
     if(question[5] == a):
-        print("Correct Answer")
+        print("Correct Answer.")
     else:
-        print(f"Incorrect, the correct answer was {question[5]}")
-        print("Better luck next time!")
-        break 
-    print(f"You won {prizes[i]}")
-    i +=1    
+         print(f"Incorrect, the correct answer was: option {question[5]}")
+         print("Better luck next time!")
+         break
+    print(f"You Won: {prizes[i]}")
+
+    sum = sum + prizes[i]
+    print(f"Your Total prize amt. is: {sum} \n") 
+
+    i += 1
